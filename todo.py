2572451c3930c5a34fcfff465a0dbf5239e403c9
Felipe De la Fuente V <fadelafuente1@gmail.com>
2018-11-23 12:55:59 -0300

    add todo class

---
+++ todo.py
@@ -0,0 +1,9 @@
+class Todo:
+  def __init__(self, title, due_date, description):
+    self.title = title
+    self.due_date = due_date
+    self.description = description
+    self.category = []
+  
+  def add_category:
+    pass
