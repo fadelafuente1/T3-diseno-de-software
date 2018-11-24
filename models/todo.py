c2c0d4509535d79f94a04dde52e206a16eef7a37
Felipe De la Fuente V <fadelafuente1@gmail.com>
2018-11-23 22:40:28 -0300

    add_category and complete_todo functionality

--- models/todo.py
+++ models/todo.py
@@ -1,13 +1,23 @@
 """
 Todo class 
 """
-class Todo:
+import datetime
 
+
+
+class Todo:
+  STATES = {'in_process': 'in_process', 'complete': 'complete'}
   def __init__(self, title, due_date, description):
     self.title = title
+    self.creation_date = datetime.datetime.now()
     self.due_date = due_date
     self.description = description
     self.category = []
+    self.state = self.STATES['in_process']
+    
+  def add_category(self, category):
+    self.category.append(category)
   
-  def add_category:
-    pass
+  def complete_todo(self):
+    self.state = self.STATES['complete']
+    return self.state
